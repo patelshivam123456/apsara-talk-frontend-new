ec03b09ff194a27c93d37e77a6d1026ab2a3b84d
shivamverma <shivamverma@securenow.in>
2026-05-21 19:46:40 +0530

    fix api url

--- constants/URLConfig.js
+++ constants/URLConfig.js
@@ -1,8 +1,8 @@
 export const config = {
-    "loginClient":process.env.NEXT_PUBLIC_APP_API_IP_URL+"authorization/auth/login",
-    "registerClient":process.env.NEXT_PUBLIC_APP_API_IP_URL+"authorization/auth/create-user",
-    "getClientProfile":process.env.NEXT_PUBLIC_APP_API_IP_URL+"authorization/client/profile-me",
-    "updateClientProfile":process.env.NEXT_PUBLIC_APP_API_IP_URL+"authorization/client/update-profile-data",
-    "deleteClientProfile":process.env.NEXT_PUBLIC_APP_API_IP_URL+"authorization/client/delete-profile-data",
-    "getAstrologersList":process.env.NEXT_PUBLIC_APP_API_IP_URL+"authorization/info/get-all-astrologers",
+    "loginClient":'https://api.apsraastro.com/'+"authorization/auth/login",
+    "registerClient":'https://api.apsraastro.com/'+"authorization/auth/create-user",
+    "getClientProfile":'https://api.apsraastro.com/'+"authorization/client/profile-me",
+    "updateClientProfile":'https://api.apsraastro.com/'+"authorization/client/update-profile-data",
+    "deleteClientProfile":'https://api.apsraastro.com/'+"authorization/client/delete-profile-data",
+    "getAstrologersList":'https://api.apsraastro.com/'+"authorization/info/get-all-astrologers",
 }
